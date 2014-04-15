73c556262b7f837171378715988783d37318fff3
Anthony Papousek <anthony@papousek.org>
2014-04-15 15:38:41 -0500

    copied models.py

--- crime/reports/models.py
+++ crime/reports/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#max_length of 50 for uncertain length
+
+class CrimeReport(models.Model):
+    incident_number = models.IntergerField()
+    reported_time = models.CharField(max_length=16)
+    status_code = models.CharField(max_length=50)
+    occurred_time = models.CharField(max_length=50)
+    building = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    incident_code = models.CharField(max_length=50)
+    stolen = models.DecimalField()
+    damaged = models.DecimalField()
+    summary = models.CharField(max_length=256)
+    
+    
+    
+
