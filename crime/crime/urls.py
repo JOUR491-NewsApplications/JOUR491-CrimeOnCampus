743fa99e4f66d1bbdbc4783f8357c4d8a5a29f42
Matt Waite <matt.waite@gmail.com>
2014-04-22 16:12:57 -0500

    adding url view and template combo for homepage and a building detail page

--- crime/crime/urls.py
+++ crime/crime/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'crime.views.home', name='home'),
+    url(r'^$', 'reports.views.home', name='home'),
+    url(r'^buildings/(?P<slug>[-\w]+)/$', 'reports.views.buildingdetail', name='buildingdetail'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- crime/reports/views.py
+++ crime/reports/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from reports.models import Building, Location, CrimeReport
 
-# Create your views here.
+def home(request):
+    buildings = Building.objects.order_by('name').distinct()
+    return render(request, 'index.html', {'buildings': buildings})
+
+def buildingdetail(request, slug):
+    building = Building.objects.get(name_slug=slug)
+    crimes = CrimeReport.objects.filter(building=building)
+    return render(request, 'buildingdetail.html', {'building': building, 'crimes': crimes,})
+
